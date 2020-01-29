7b776476eb6cd4a5585e6e784eb9abf02e072105
Valerie <valerie.flores64@gmail.com>
2020-01-28 16:14:28 -0800

    created starter files

---
+++ app.js
@@ -0,0 +1,8 @@
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const sequelize = require("sequelize");
+
+// Chalk Test
+console.log(chalk.blue.bold('Hello world!'));
+console.log(chalk.bgRed('Hello world!'));
+
